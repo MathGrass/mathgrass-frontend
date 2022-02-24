10bc3e45773f0bab0e2b784bc9a14913c377881a
Andreas Domanowski <andreas@domanowski.net>
2022-02-24 13:39:57 +0100

    dev: TS-ify Assessment component

--- src/Components/Assessment/Assessment.tsx
+++ src/Components/Assessment/Assessment.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Form from "@rjsf/core";
+import { JSONSchema7 } from "json-schema";
+
+function submitStudentAssessment() : void {
+
+}
 
 class Assessment extends React.Component {
 
     render() {
-        const schema= {
+        const schema : JSONSchema7 = {
             title: "Graph Assessment",
             type: "object",
             required: ["isPlanar"],
@@ -20,13 +25,10 @@ class Assessment extends React.Component {
         };
 
 
-        const log = (type) => console.log.bind(console, type);
 
         return (<Form schema={schema}
                       uiSchema={uiSchema}
-                      onChange={log("changed")}
-                      onSubmit={log("submitted")}
-                      onError={log("errors")}/>);
+                      onSubmit={submitStudentAssessment}/>);
     }
 }
 
--- src/MathGrassEditor.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import MathGrass from './MathGrass';
-
-test('renders learn react link', () => {
-  render(<MathGrass />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
