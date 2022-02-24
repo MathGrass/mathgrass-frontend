1ff09c013703263256cabf8d9da275614e76a521
Andreas Domanowski <andreas@domanowski.net>
2022-02-24 11:28:13 +0100

    dev: Add RJSF support, Add prototypical form

--- src/Components/AssessmentComponent/AssessmentComponent.jsx
+++ src/Components/AssessmentComponent/AssessmentComponent.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import Form from "@rjsf/core";
 
 class AssessmentComponent extends React.Component {
+
     render() {
-        return(<div>Form goes here.</div>);
+        const schema= {
+            title: "Graph Assessment",
+            type: "object",
+            required: ["isPlanar"],
+            properties: {
+                isPlanar: {type: "boolean", title: "Is the graph planar?"}
+            }
+        };
+
+        const uiSchema =  {
+            isPlanar: {
+                "ui:widget": "radio"
+            }
+        };
+
+
+        const log = (type) => console.log.bind(console, type);
+
+        return (<Form schema={schema}
+                      uiSchema={uiSchema}
+                      onChange={log("changed")}
+                      onSubmit={log("submitted")}
+                      onError={log("errors")}/>);
     }
 }
 
