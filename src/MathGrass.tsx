8866da99139593aadd2376fb56caca7c806cba47
Andreas Domanowski <andreas@domanowski.net>
2022-02-25 15:31:35 +0100

    feat: introduce a layout

--- src/MathGrass.tsx
+++ src/MathGrass.tsx
@@ -4,18 +4,59 @@ import Assessment from "./Components/Assessment/Assessment";
 import IncrementalHints from "./Components/IncrementalHints/IncrementalHints";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 class MathGrass extends React.Component {
-    render(){
+    render() {
         return (
-            <div className='row'>
-                <div className='col-10 border'>
-                    <GraphEditor />
+            <div className="m-2">
+                <div className="row">
+                    <div className="col-md-8 mb-1">
+                        <div className="card">
+                            <div className="card-header">
+                                <h2>Graph</h2>
+                            </div>
+                            <div className="card-body">
+                                <GraphEditor/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 mb-1">
+                        <div className="row">
+                            <div className="col-md-12 mb-1">
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h2>Tasks</h2>
+                                    </div>
+                                    <div className="card-body">
+                                        Task Management
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-12 mb-1">
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h2>Assessment</h2>
+                                    </div>
+                                    <div className="card-body">
+                                        <Assessment/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-12 mb-1">
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h2>Hints</h2>
+                                    </div>
+                                    <div className="card-body">
+                                        <IncrementalHints/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className='col-2 border'>
-                    <Assessment />
-                    <IncrementalHints />
-                </div>
+
+
+
             </div>
 
         );
