56c1aad8c383095a5e29ad2b29c953886cf8e586
Andreas Domanowski <andreas@domanowski.net>
2022-02-25 13:37:36 +0100

    feat: add styling with bootstrap

--- src/MathGrass.tsx
+++ src/MathGrass.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import GraphEditor from "./Components/GraphEditor/GraphEditor";
 import Assessment from "./Components/Assessment/Assessment";
 import IncrementalHints from "./Components/IncrementalHints/IncrementalHints";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class MathGrass extends React.Component {
     render(){
         return (
-            <div>
-                <GraphEditor />
-                <Assessment />
-                <IncrementalHints />
+            <div className='row'>
+                <div className='col-10 border'>
+                    <GraphEditor />
+                </div>
+                <div className='col-2 border'>
+                    <Assessment />
+                    <IncrementalHints />
+                </div>
             </div>
 
         );
