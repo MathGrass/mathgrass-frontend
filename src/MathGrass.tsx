210f658e4f95a78470260f151982b7c08a98122f
Andreas Domanowski <andreas@domanowski.net>
2022-02-25 16:14:18 +0100

    dev: Comment div structure in MathGrass template

--- src/MathGrass.tsx
+++ src/MathGrass.tsx
@@ -8,8 +8,11 @@ import TaskManagement from "./Components/TaskManagement/TaskManagement";
 class MathGrass extends React.Component {
     render() {
         return (
-            <div className="m-2">
+            /*main container*/
+            <div className="container-fluid m-2">
+                {/*main row containing graph and sidebar*/}
                 <div className="row">
+                    {/*graph*/}
                     <div className="col-md-9 mb-1">
                         <div className="card">
                             <div className="card-header">
@@ -20,42 +23,40 @@ class MathGrass extends React.Component {
                             </div>
                         </div>
                     </div>
+                    {/*container for sidebar menu*/}
                     <div className="col-md-3 mb-1">
-                        <div className="row">
-                            <div className="col-md-12 mb-1">
-                                <div className="card">
-                                    <div className="card-header">
-                                        <h2>Tasks</h2>
-                                    </div>
-                                    <div className="card-body">
-                                        <TaskManagement />
-                                    </div>
+                        <div className="col-md-12 mb-1">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h2>Tasks</h2>
+                                </div>
+                                <div className="card-body">
+                                    <TaskManagement/>
                                 </div>
                             </div>
-                            <div className="col-md-12 mb-1">
-                                <div className="card">
-                                    <div className="card-header">
-                                        <h2>Assessment</h2>
-                                    </div>
-                                    <div className="card-body">
-                                        <Assessment/>
-                                    </div>
+                        </div>
+                        <div className="col-md-12 mb-1">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h2>Assessment</h2>
+                                </div>
+                                <div className="card-body">
+                                    <Assessment/>
                                 </div>
                             </div>
-                            <div className="col-md-12 mb-1">
-                                <div className="card">
-                                    <div className="card-header">
-                                        <h2>Hints</h2>
-                                    </div>
-                                    <div className="card-body">
-                                        <IncrementalHints/>
-                                    </div>
+                        </div>
+                        <div className="col-md-12 mb-1">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h2>Hints</h2>
+                                </div>
+                                <div className="card-body">
+                                    <IncrementalHints/>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
 
 
             </div>
