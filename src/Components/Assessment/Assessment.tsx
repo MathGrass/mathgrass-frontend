b29441b0c38c09d4aabbee9b7d39ea900fd567e7
Andreas Domanowski <andreas@domanowski.net>
2022-02-28 15:40:03 +0100

    dev: add state to Assessment component

--- src/Components/Assessment/Assessment.tsx
+++ src/Components/Assessment/Assessment.tsx
@@ -1,33 +1,47 @@
 import React from "react";
-import Form from "@rjsf/core";
+import Form, {ISubmitEvent, UiSchema} from "@rjsf/core";
 import {JSONSchema7} from "json-schema";
 
-function submitStudentAssessment(): void {
+type AssessmentProps = {}
 
+type AssessmentState = {
+    schema : JSONSchema7
+    uiSchema: UiSchema
 }
 
-class Assessment extends React.Component {
+function submitStudentSolution(form: ISubmitEvent<any>): void {
+    console.log(form.formData)
+}
 
-    render() {
-        const schema: JSONSchema7 = {
+function getInitialState() : AssessmentState {
+    return {
+        schema : {
             title: "Graph Assessment",
             type: "object",
             required: ["isPlanar"],
             properties: {
                 isPlanar: {type: "boolean", title: "Is the graph planar?"}
             }
-        };
-
-        const uiSchema = {
+        },
+        uiSchema : {
             isPlanar: {
                 "ui:widget": "radio"
             }
-        };
+        }
+    }
+}
 
+class Assessment extends React.Component<AssessmentProps, AssessmentState> {
 
-        return (<Form schema={schema}
-                      uiSchema={uiSchema}
-                      onSubmit={submitStudentAssessment}/>);
+    constructor(props : AssessmentProps, state : AssessmentState) {
+        super(props);
+        this.state = getInitialState();
+    }
+
+    render() {
+        return (<Form schema={this.state.schema}
+                      uiSchema={this.state.uiSchema}
+                      onSubmit={submitStudentSolution}/>);
     }
 }
 
