a3867b0e5cd14b6a456680f8d0d132ffd949f02b
Andreas Domanowski <andreas@domanowski.net>
2022-03-03 11:40:31 +0100

    fix: Initialize Editor after mounting the GraphEditor component

--- src/components/GraphEditor/GraphEditor.tsx
+++ src/components/GraphEditor/GraphEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import * as joint from 'jointjs'
 
 type GraphEditorProps = {}
@@ -12,42 +12,44 @@ const GRAPH_CONTAINER_ID = 'mathGrassEditor';
 const EDITOR_WIDTH_SCALING_FACTOR = 0.95;
 
 const GraphEditor = () => {
-
     let namespace = joint.shapes;
     let graph = new joint.dia.Graph({}, {cellNamespace: namespace});
 
-    const domContainer = document.getElementById(GRAPH_CONTAINER_ID);
+    useEffect(() => {
+        const domContainer = document.getElementById(GRAPH_CONTAINER_ID);
 
-    new joint.dia.Paper({
-        el: domContainer!,
-        model: graph,
-        width: EDITOR_WIDTH_SCALING_FACTOR * domContainer!.offsetWidth,
-        height: 500,
-        gridSize: 1,
-        cellViewNamespace: namespace,
-        restrictTranslate: true
-    });
+        new joint.dia.Paper({
+            el: domContainer!,
+            model: graph,
+            width: EDITOR_WIDTH_SCALING_FACTOR * domContainer!.offsetWidth,
+            height: 500,
+            gridSize: 1,
+            cellViewNamespace: namespace,
+            restrictTranslate: true
+        });
 
-    let circle = new joint.shapes.standard.Circle();
-    circle.position(100, 30);
-    circle.resize(40, 40);
-    circle.attr({
-        body: {},
-        label: {
-            text: '1',
-        }
-    });
-    circle.addTo(graph);
+        let circle = new joint.shapes.standard.Circle();
+        circle.position(100, 30);
+        circle.resize(40, 40);
+        circle.attr({
+            body: {},
+            label: {
+                text: '1',
+            }
+        });
+        circle.addTo(graph);
 
-    let circle2 = circle.clone();
-    circle2.translate(300, 0);
-    circle2.attr('label/text', '2');
-    circle2.addTo(graph);
+        let circle2 = circle.clone();
+        circle2.translate(300, 0);
+        circle2.attr('label/text', '2');
+        circle2.addTo(graph);
 
-    let link = new joint.shapes.standard.Link();
-    link.source(circle);
-    link.target(circle2);
-    link.addTo(graph);
+        let link = new joint.shapes.standard.Link();
+        link.source(circle);
+        link.target(circle2);
+        link.addTo(graph);
+    })
+
     return (<div id={GRAPH_CONTAINER_ID}>Graph</div>)
 }
 
