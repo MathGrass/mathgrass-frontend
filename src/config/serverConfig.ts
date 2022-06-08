96c726c5172d3eef5a6d36515349c68bb7159ab3
Andreas Domanowski <andreas@domanowski.net>
2022-06-08 10:53:49 +0200

    dev: Refactor server config interface

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -1,3 +1,10 @@
+export interface MathGrassConfig {
+    readonly serverUrl: string;
+    readonly taskTypesQueryPath: string;
+    readonly submitAssessmentPath: string;
+    readonly requestHintsPath: string;
+}
+
 let serverUrl: string;
 
 export function setServerUrl(url: string): void {
