38b47ab8169c557e13b0dbf857f30873ee0c2275
Andreas Domanowski <andreas@domanowski.net>
2022-06-09 08:48:55 +0200

    dev: Extract initial demo state

--- src/state/applicationState.tsx
+++ src/state/applicationState.tsx
@@ -4,24 +4,24 @@ import {JSONSchema7} from 'json-schema';
 import {UiSchema} from '@rjsf/core';
 import {getDemoAssessmentSchema} from './initialResources/demoQuestionSchema';
 import {generateDemoGraph} from './initialResources/demoGraph';
+import {getDemoTaskTypes} from './initialResources/demoTaskTypes';
 
-interface TaskState {
+interface ApplicationState {
     taskType: string;
     taskId: string;
     graphUneditedOriginal: any;
     graphInEditor: any;
-    questionSchema: AssessmentSchema;
+    questionSchema: JsonFormTuple;
     hintLevel: number;
-    availableTasks: { identifier: string; displayName: string; } [];
+    availableTasks: { identifier: string; displayName: string; }[];
 }
 
-
-export interface AssessmentSchema {
+export interface JsonFormTuple {
     assessmentSchema: JSONSchema7;
     assessmentUiSchema: UiSchema;
 }
 
-function getInitialTaskState(): TaskState {
+function getInitialApplicationState(): ApplicationState {
     return {
         taskType: 'randomTaskType',
         taskId: 'randomId',
@@ -33,13 +33,7 @@ function getInitialTaskState(): TaskState {
     };
 }
 
-export interface JSONSchemaEnumType {
-    'enum': string;
-    'title': string;
-}
-
-
-const initialTaskState: TaskState = getInitialTaskState();
+const initialTaskState: ApplicationState = getInitialApplicationState();
 export const applicationState = createSlice({
     name: 'tasks',
     initialState: initialTaskState,
@@ -54,17 +48,6 @@ export const applicationState = createSlice({
         }
     }
 });
-
-function getDemoTaskTypes() {
-    return [{
-        identifier: 'planarity',
-        displayName: 'Planarity'
-    }, {
-        identifier: 'bipartite',
-        displayName: 'Bipartite Graphs'
-    }];
-}
-
 
 export const { requestNewGraph, propagateGraphState } = applicationState.actions;
 export default applicationState.reducer;
