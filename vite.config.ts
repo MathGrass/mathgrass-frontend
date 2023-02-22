6347cae8a8859dae4fc24934842adc542a3a9f7e
Andreas Domanowski <andreas@domanowski.net>
2023-02-22 15:49:22 +0100

    chore: Serve dev-server under port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        port: 3000
+    },
     plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
 });
