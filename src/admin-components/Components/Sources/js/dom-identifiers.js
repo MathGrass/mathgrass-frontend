29fca6a2dfd2159aa63de71a851ceeb987884701
Sreeram Ramakrishnan <sreesumi007@gmail.com>
2023-04-17 12:41:27 +0200

    mathgrass-admin-frontend

---
+++ src/admin-components/Components/Sources/js/dom-identifiers.js
@@ -0,0 +1,17 @@
+export const dom_itemdifier_ctxMenu = 'contextMenu';
+export const dom_itemdifier_rectTrigger = 'contextmenu-addRect-trigger';
+export const dom_itemdifier_cicleTrigger = 'contextmenu-addCircle-trigger';
+
+export const SaveGraph = "saveGraphJson";
+export const ClearGraph = "clearGraphView"
+
+export const diagramCanvas = "diagramCanvas";
+
+export const graphChange = "graphChange";
+export const closeGraphicalHint = "closeGraphicalHint";
+
+
+
+
+
+
--- src/state/common/store.tsx
+++ src/state/common/store.tsx
@@ -1,9 +1,24 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {applicationState} from '../applicationState';
-
+// Change for Admin Interface - Starts
+import appCommonSlice from "../../admin-components/store/adminAppCommonOperations";
+import adminAppJSONFormation from "../../admin-components/store/adminAppJSONFormation";
+import hintsWithOrderSlice from "../../admin-components/store/slices/hintsWithOrderSlice";
+import loginAuthenticationSlice from "../../admin-components/store/slices/loginAuthenticationSlice";
+import quesMultipleChoiceSlice from "../../admin-components/store/slices/quesMultipleChoiceSlice";
+import textHintSlice from "../../admin-components/store/slices/textHintSlice";
+// Change for Admin Interface - Ends
 export const store = configureStore({
     reducer: {
         applicationStateManagement: applicationState.reducer,
+        // Change for Admin Interface - Starts
+        appCommonSlice,
+        textHintSlice,
+        quesMultipleChoiceSlice,
+        hintsWithOrderSlice,
+        adminAppJSONFormation,
+        loginAuthenticationSlice
+        // Change for Admin Interface - Ends
     },
 });
 
