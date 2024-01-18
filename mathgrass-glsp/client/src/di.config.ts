200f5964ed436276b330821f57d0fec40d759621
DelphiXE5 <flossyna@outlook.de>
2024-01-18 02:49:37 +0100

    Working state for glsp client

--- mathgrass-glsp/client/src/di.config.ts
+++ mathgrass-glsp/client/src/di.config.ts
@@ -13,7 +13,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import { createWorkflowDiagramContainer } from '@mathgrass-glsp/diagram-definition';
 import {
     ConsoleLogger,
     IDiagramOptions,
@@ -25,6 +24,7 @@ import {
     createDiagramOptionsModule,
     toolPaletteModule
 } from '@eclipse-glsp/client';
+import { createWorkflowDiagramContainer } from '@mathgrass-glsp/diagram-definition';
 import { Container } from 'inversify';
 import '../css/diagram.css';
 export default function createContainer(options: IDiagramOptions): Container {
--- mathgrass-glsp/diagram-definition/src/workflow-views.tsx
+++ mathgrass-glsp/diagram-definition/src/workflow-views.tsx
@@ -14,15 +14,15 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 import {
-    angleOfPoint,
-    findParentByFeature,
-    getSubType,
+    GEdge,
     Point,
     PolylineEdgeViewWithGapsOnIntersections,
     RenderingContext,
-    GEdge,
-    setAttr,
     ShapeView,
+    angleOfPoint,
+    findParentByFeature,
+    getSubType,
+    setAttr,
     svg,
     toDegrees
 } from '@eclipse-glsp/client';
@@ -75,7 +75,7 @@ export class IconView extends ShapeView {
                 'M9.1 4.4L8.6 2H7.4l-.5 2.4-.7.3-2-1.3-.9.8 1.3 2-.2.7-2.4.5v1.2l2.4.5.3.8-1.3 2 .8.8 2-1.3.8.3.4 2.3h1.2l.5-2.4.8-.3 2 1.3.8-.8-1.3-2 .3-.8 2.3-.4V7.4l-2.4-.5-.3-.8 1.3-2-.8-.8-2 1.3-.7-.2zM9.4 1l.5 2.4L12 2.1l2 2-1.4 2.1 2.4.4v2.8l-2.4.5L14 12l-2 2-2.1-1.4-.5 2.4H6.6l-.5-2.4L4 13.9l-2-2 1.4-2.1L1 9.4V6.6l2.4-.5L2.1 4l2-2 2.1 1.4.4-2.4h2.8zm.6 7c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zM8 9c.6 0 1-.4 1-1s-.4-1-1-1-1 .4-1 1 .4 1 1 1z';
         }
 
-        let vnode = (
+        const vnode = (
             <g>
                 <path
                     transform={'scale(1.15),translate(0.75,0.75)'}
@@ -88,9 +88,10 @@ export class IconView extends ShapeView {
             </g>
         );
 
+        console.log("test2");
+
         const subType = getSubType(element);
         if (subType) {
-            vnode.data = {};
             setAttr(vnode, 'class', subType);
         }
 
